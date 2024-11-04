9aa634c47603bc6b4e326ee6d1c992157068e2ac
Cynlrs <cyn.lrs@nus.edu.sg>
2024-11-04 17:03:44 +0800

    First commit for YST

---
+++ data.js
@@ -0,0 +1,218 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-alice-lee-plaza",
+      "name": "Alice Lee Plaza",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.4496869209839787,
+        "pitch": -0.23162572275029447,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.167842052860106,
+          "pitch": -0.04024468832005468,
+          "rotation": 4.71238898038469,
+          "target": "1-conservatory-foyer"
+        },
+        {
+          "yaw": -2.989192689844085,
+          "pitch": 0.0033312039821531414,
+          "rotation": 4.71238898038469,
+          "target": "1-conservatory-foyer"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.0162363417579483,
+          "pitch": -0.3656497608242155,
+          "title": "NUS Museum",
+          "text": "NUS Museum hosts a wide range of exhibitions, collaborations and programmes to create space and opportunities for cultural and interdisciplinary conversations within NUS and beyond."
+        },
+        {
+          "yaw": 1.0946123767831715,
+          "pitch": 0.07567577883313348,
+          "title": "Nearest shuttle service",
+          "text": "Turn left for nearest shuttle bus services for convenient commute within campus."
+        },
+        {
+          "yaw": -2.1067303311064354,
+          "pitch": -0.16769510555203127,
+          "title": "Side entrance",
+          "text": "Side entrance to Yong Siew Toh Conservatory of Music."
+        },
+        {
+          "yaw": -2.92316329531743,
+          "pitch": -0.07763839924772498,
+          "title": "Entrance",
+          "text": "Entrance of Yong Siew Toh Conservatory of Music"
+        },
+        {
+          "yaw": -2.5162847107909947,
+          "pitch": -0.09657151361891891,
+          "title": "Yong Siew Toh Conservatory of Music",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-conservatory-foyer",
+      "name": "Conservatory Foyer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 3.0559414974734906,
+        "pitch": 0.09779838696865184,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7621459863716051,
+          "pitch": 0.0017579988953215775,
+          "rotation": 0,
+          "target": "0-alice-lee-plaza"
+        },
+        {
+          "yaw": -3.0280318264571253,
+          "pitch": 0.17060851888144946,
+          "rotation": 13.351768777756625,
+          "target": "2-conservatory-lounge"
+        },
+        {
+          "yaw": 2.7218236611625315,
+          "pitch": 0.18380801852771,
+          "rotation": 5.497787143782138,
+          "target": "0-alice-lee-plaza"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.7574198185752543,
+          "pitch": -0.14360414435861557,
+          "title": "Side exit",
+          "text": "Side exit to Alice Lee Plaza"
+        },
+        {
+          "yaw": -2.93726007426344,
+          "pitch": 0.05115329157383108,
+          "title": "Conservatory Lounge",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.99622239074591,
+          "pitch": -0.024662001814515833,
+          "title": "Music Library",
+          "text": "Music library homes a collection of print and electronic materials including music scores, CD and DVD titles.&nbsp;There are state-of-the-art listening and multimedia viewing stations."
+        },
+        {
+          "yaw": 2.6946491357014626,
+          "pitch": 0.08589473555878513,
+          "title": "Exit",
+          "text": "Head to exit to Alice Lee Plaza and NUS Museum"
+        }
+      ]
+    },
+    {
+      "id": "2-conservatory-lounge",
+      "name": "Conservatory Lounge",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -3.1272890262152107,
+        "pitch": -0.18630634336786578,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3401456136422958,
+          "pitch": 0.11463223001363154,
+          "rotation": 0,
+          "target": "1-conservatory-foyer"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.9996083998728764,
+          "pitch": -0.02133391238326432,
+          "title": "Foreword Coffee Roasters",
+          "text": "The conservatory lounge is now a cafe."
+        },
+        {
+          "yaw": -0.23514614120047206,
+          "pitch": 0.20173737204669173,
+          "title": "Back to Foyer",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.182559227478464,
+          "pitch": -0.2004190174906082,
+          "title": "History of the conservatory",
+          "text": "The pictures are still found in the current cafe to showcase the history of the conservatory."
+        }
+      ]
+    }
+  ],
+  "name": "Yong Siew Toh Conservatory of Music",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
